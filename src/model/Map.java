f8f41ef38799b0973ba14643e4bb73633a161b71
PierreDerathe <pierre.derathe@u-psud.fr>
2023-02-02 17:40:38 +0100

    Début de la construction de Map

--- src/model/Map.java
+++ src/model/Map.java
@@ -1,10 +1,23 @@
 package model;
 
 public class Map {
-    private final MainModel model;
+//	Taille de la grille
+	public static final int sizeGrid = 20;
+    private final MainModel m;
     private Cell[][] grid;
 
     public Map(MainModel model) {
-        this.model = model;
+        this.m = model;
+        grid = new Cell[sizeGrid][sizeGrid];
+    }
+    
+    private void initGrid(){
+    	for(int i = 0; i < sizeGrid; i++)
+    		for(int j = 0; j < sizeGrid; j++)
+    			grid[i][j] = createCell();
+    }
+    
+    private Cell createCell() {
+    	return new Cell(m);
     }
 }
