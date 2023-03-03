c74bb124696eee2ce66b82b615e4eb5ce8cab82e
_main_ <_main_@serge-hppavilionlaptop15zcw100>
2023-03-03 20:41:02 +0100

    A* started, need lots of testing

--- src/model/MobileEntity.java
+++ src/model/MobileEntity.java
@@ -2,6 +2,9 @@ package model;
 
 import model.terrain.Cell;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 public class MobileEntity implements IEntity {
 	private MainModel model;
 	private Cell position;
@@ -68,6 +71,69 @@ public class MobileEntity implements IEntity {
 		return c.equals(this.position);
 	}
 	
+	private int abs(int a) {
+		return a > 0 ? a : -a;
+	}
+	
+	private int calcG(Cell a, Cell b) {
+		int res = this.abs(a.getX() - b.getX());
+		return res + abs(a.getY() - b.getY());
+	}
+	
+	private int dist(int ax, int ay, int bx, int by) {
+		int res = this.abs(ax - bx);
+		return res + abs(ay - by);
+	}
+	
+	private void calcG(int G[][], int H[][], int F[][], int ax, int ay, int bx, int by) {
+        G[ax+1][ay] = dist(ax+1, ay, ax, ay);
+        H[ax+1][ay] = dist(ax+1, ay, bx, by);
+        F[ax+1][ay] = G[ax+1][ay] + H[ax+1][ay];
+		
+		G[ax-1][ay] = dist(ax-1, ay, ax, ay);
+		H[ax-1][ay] = dist(ax-1, ay, bx, by);
+		F[ax-1][ay] = G[ax-1][ay] + H[ax-1][ay];
+		
+		G[ax][ay+1] = dist(ax, ay+1, ax, ay);
+		H[ax][ay+1] = dist(ax, ay+1, bx, by);
+		F[ax][ay+1] = G[ax][ay+1] + H[ax][ay+1];
+		
+		G[ax][ay-1] = dist(ax, ay-1, ax, ay);
+		H[ax][ay-1] = dist(ax, ay-1, bx, by);
+		F[ax][ay-1] = G[ax][ay-1] + H[ax][ay-1];
+	}
+	
+//	as i see it, we'd like the method to return an array of `Cell`s
+//	so the entity just follows the one path
+//	in this case the method may not be here...
+//	in the meantime, i'll start here
+//	Best Regards.
+//	Serge
+	public boolean aStar(Cell dest) {
+		int depX = this.position.getX();
+		int depY = this.position.getY();
+		
+		int arrX = dest.getX();
+		int arrY = dest.getY();
+//		we need another map with three numbers in it, starting with the cells surrounding the starting point::
+//		* the distance of each cell from the starting point (G cost)
+//		* the distance of each cell from the arriving point (H cost)
+//		* the sum G + H of each cell (F cost)
+//		ArrayList<ArrayList<int[]>> values = new ArrayList<>(this.map.sizeGrid);  
+		int G[][] = new int[this.map.sizeGrid][this.map.sizeGrid];
+		int H[][] = new int[this.map.sizeGrid][this.map.sizeGrid];
+		int F[][] = new int[this.map.sizeGrid][this.map.sizeGrid];
+		
+		
+		
+//		next we will choose the cell containing the samllest F cost and will choose as the new starting point
+		boolean notFound = true;
+		while(notFound) {
+			
+			
+		}
+		return false;
+	}
 
 	//	getters
 	public Action getCurrentAction() {return this.currentAction;}
