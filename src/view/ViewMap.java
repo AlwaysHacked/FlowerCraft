38674c2edd0eb0c274fe7b68df153b817b849081
Lola Austin <lola.austin@universite-paris-saclay.fr>
2023-03-09 00:03:08 +0100

    View WIP

--- src/view/ViewMap.java
+++ src/view/ViewMap.java
@@ -1,5 +1,23 @@
 package view;
+import model.*;
 
-public class ViewMap {
+import java.awt.Dimension;
+import java.util.ArrayList;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.MainModel;
+
+public class ViewMap extends JPanel {
+	private MainModel m;
+	private ArrayList<JLabel> jl = new ArrayList<>();
+	public final int SIZE = 100;
+	
+	public ViewMap(MainModel model) {
+		this.m = model;
+		Dimension dim = new Dimension(SIZE, SIZE); /////change later on
+        this.setPreferredSize(dim);
+        setOpaque(true);
+	}
 }
