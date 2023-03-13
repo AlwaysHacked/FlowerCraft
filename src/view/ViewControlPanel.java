a5fb7ec6ee6f786e05b2b826259c826c0ca5d371
lola.austin <lola.austin>
2023-03-13 16:25:27 +0100

    fix

---
+++ src/view/ViewControlPanel.java
@@ -0,0 +1,59 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.MainModel;
+
+public class ViewControlPanel extends JPanel {
+	
+	private MainModel m;
+	private ArrayList<JLabel> elements;
+	private final int width = ViewMap.STEP*3;
+    private final int height = ViewMap.STEP*10;
+	
+	public ViewControlPanel (MainModel model) {
+
+		this.m = model;
+		this.elements = new ArrayList<>();
+
+        Dimension dim = new Dimension(width, height);
+        this.setPreferredSize(dim);
+        setOpaque(true);
+
+        ImageIcon temp = new ImageIcon();
+        JLabel object = new JLabel();
+        object.setBounds(0, 0, width, height/3);
+        object.setIcon(temp);
+
+        //GrilleController ctrl = new GrilleController(grille);
+        // /** Enregistrement du contrôleur comme auditeur du bouton.**
+        //object.addMouseListener(ctrl);
+        this.elements.add(object);
+
+    }
+
+    public void update() {
+        // TODO Auto-generated method stub
+        repaint();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paint(g);
+
+    }
+
+    public void paint(Graphics g) {
+
+        
+
+    }
+
+
+}
