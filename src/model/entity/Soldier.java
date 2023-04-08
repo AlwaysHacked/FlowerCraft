1ec8162c0dfb36b33e7e77985fc4f4725bda4dc9
Jiren <jiren.ren@universite-paris-saclay.fr>
2023-04-08 21:45:18 +0200

    Soilder changes but not sure

--- src/model/entity/Soldier.java
+++ src/model/entity/Soldier.java
@@ -30,10 +30,10 @@ public class Soldier extends Entity implements IEntity {
 	}
 
 	
-	public void move() {
-		if (destination == position) {
-			destination = (ICell) model.camps.get(rand.nextInt(model.camps.size()));
-		} else{
+	//public void move() {
+	//	if (destination == position) {
+	//		destination = (ICell) model.camps.get(rand.nextInt(model.camps.size()));
+	//	} else{
 			
 			// if (path.empty()) generatePath();
 			// if (canMove(this.path.peek())) {
@@ -41,68 +41,40 @@ public class Soldier extends Entity implements IEntity {
 			// 	this.position = this.path.pop();
 			// 	this.position.addEntity(this);
 			// } else generatePath();
+	//	}
+	//}
+    
+	//go to aleartoire camps(n'importe lequele)
+	@Override
+	public void move() {
+		destination = (ICell) model.camps.get(rand.nextInt(model.camps.size()));
+			
+			if (path.empty()) {
+				generatePath();
+			} else {
+				for(var i : path)System.out.println(i.getCoord());
+				if (canMove(this.path.peek())) {
+					this.path.peek().addEntity(this);
+					this.position.deleteEntity();
+					this.position = this.path.peek();
+					this.path.pop();///////
+
+				} // else generatePath();
+			}
 		}
 	}
+
+
 
 	@Override
 	public boolean isEnemy(IEntity ent) {
            return ent instanceof Navi || ent instanceof Camp;
 	}
-	
-	// before solider go to find Camp, he check if there is navi or camp next to him.
-	public void check(){
-    //place all cases ordonnee
-		int xR = super.position.getX()+1;
-		int yR = super.position.getY();
-
-		int xU = super.position.getX();
-		int yU = super.position.getY()+1;
-
-		int xL = super.position.getX()-1;
-		int yL = yR;
-
-		int xD = xU;
-		int yD = super.position.getY()-1;
-
-		Cell cellR = new Cell(model,xR,yR);
-		Cell cellU = new Cell(model,xU,yU);
-		Cell cellL = new Cell(model,xL,yL);
-		Cell cellD = new Cell(model,xD,yD);
-
-	//check the camp or navi is in those positions or no
-        
-	//     if(cellR.getEntity() == navi || cellR.getEntity() == camp||cellD.getEntity() == navi ||cellD.getEntity() == camp)this.attack();
-	// 	if(cellL.getEntity() == navi || cellL.getEntity() == camp || cellU.getEntity() == navi||cellU.getEntity() == camp)this.attack();
-	 }
-
-	// search for the camp
-
-	public ICell search(){
-
-		ArrayList<Camp> camps = super.model.getCamps();
-
-		int distance = 700;
-        
-		for(Camp camp:camps){
-
-			ICell campP = camp.position;// get each Camp Position
-		// utiliser AStar pour calculer la plus proche camp avec soldier,
-//            this.position;//position of soldier.
-		// stock le temps-distance with a temps number(il va changer avec le for)
-		    int n = 0;
-
-			if(n<distance) distance = n;//distance gets the best closed between the solider and camp
-		}
-
-		//how we know which camp has the best distance with int distance?
-		ICell campPosition = null;
-
-		return campPosition;
-        
-	}
 
 	public void SoilderAction(){
-     //het the list of positions next to the soilder   
+		int nb = 0;
+	// before solider go to find Camp, he check if there is navi or camp next to him.
+     //have the list of positions next to the soilder   
 		ArrayList<ICell>n = super.map.neighbours(super.position);
     //check the neighbours is enemy or not
 		for(int i = 0; i<n.size();i++){
@@ -110,17 +82,17 @@ public class Soldier extends Entity implements IEntity {
 			if(n.get(i).getEntity() != null){
                this.canAttack();
 			   n.get(i).getEntity().sufferAttack(this.attack);
+			   break;//because soilder only can attack once after soilder"s attack , 
+			   //it"s navi's turn
+			   nb = nb + 1;
 			}
 		}
-
-	//go to search the camp and get the camp position
-    
-	// ICell p = this.search();
-
-	//move to the campS
+	//and then if 4 cells around soilder is null he goes to aleatoire camp.
+        if(nb == 0)this.move(); //nb == 0 so all the cells are null /vide
 
 	}
-
+	
+	
 	@Override
 	public String toString() {
 		return "s";
